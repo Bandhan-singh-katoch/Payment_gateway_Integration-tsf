97f8fee8736020e0eabafff74a573ffe435021b3
Bandhan singh <parbhatbandhan@gmail.com>
2021-08-08 18:23:38 +0530

    created the app

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,21 +1,15 @@
-# Add project specific ProGuard rules here.
-# You can control the set of applied configuration files using the
-# proguardFiles setting in build.gradle.
-#
-# For more details, see
-#   http://developer.android.com/guide/developing/tools/proguard.html
+-keepclassmembers class * {
+    @android.webkit.JavascriptInterface <methods>;
+}
 
-# If your project uses WebView with JS, uncomment the following
-# and specify the fully qualified class name to the JavaScript interface
-# class:
-#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
-#   public *;
-#}
+-keepattributes JavascriptInterface
+-keepattributes *Annotation*
 
-# Uncomment this to preserve the line number information for
-# debugging stack traces.
-#-keepattributes SourceFile,LineNumberTable
+-dontwarn com.razorpay.**
+-keep class com.razorpay.** {*;}
 
-# If you keep the line number information, uncomment this to
-# hide the original source file name.
-#-renamesourcefileattribute SourceFile
+-optimizations !method/inlining/*
+
+-keepclasseswithmembers class * {
+  public void onPayment*(...);
+}
--- app/src/main/java/com/example/donationapp/MainActivity.kt
+++ app/src/main/java/com/example/donationapp/MainActivity.kt
@@ -1,28 +1,75 @@
 package com.example.donationapp
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.donationapp.databinding.ActivityMainBinding
 import com.razorpay.Checkout
+import com.razorpay.PaymentResultListener
+import org.json.JSONObject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PaymentResultListener {
 
+    val TAG:String = MainActivity::class.toString()
     lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Checkout.preload(applicationContext)
+
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        Checkout.preload(getApplicationContext());
+
 
         binding.donateButton.setOnClickListener {
             makeDonation()
         }
     }
 
-    private fun makeDonation(){
+    private fun makeDonation() {
 
+        val activity: Activity = this
+        val co = Checkout()
+
+        try {
+            val options = JSONObject()
+            options.put("name","Razorpay Corp")
+            options.put("description","Demoing Charges")
+            options.put("image","https://s3.amazonaws.com/rzp-mobile/images/rzp.png")
+            options.put("currency","INR")
+            options.put("amount","80000")
+            options.put("send_sms_hash",true);
+
+            val prefill = JSONObject()
+            prefill.put("email","test@razorpay.com")
+            prefill.put("contact","9021066696")
+
+            options.put("prefill",prefill)
+            co.open(activity,options)
+        }catch (e: Exception){
+            Toast.makeText(activity,"Error in payment: "+ e.message,Toast.LENGTH_LONG).show()
+            e.printStackTrace()
+        }
+    }
+
+    override fun onPaymentError(errorCode: Int, response: String?) {
+        try{
+            Toast.makeText(this,"Payment failed $errorCode \n $response",Toast.LENGTH_LONG).show()
+        }catch (e: Exception){
+            Log.e(TAG,"Exception in onPaymentSuccess", e)
+        }
+    }
+
+    override fun onPaymentSuccess(razorpayPaymentId: String?) {
+        try{
+            Toast.makeText(this,"Payment Successful $razorpayPaymentId",Toast.LENGTH_LONG).show()
+        }catch (e: Exception){
+            Log.e(TAG,"Exception in onPaymentSuccess", e)
+        }
     }
 
 }
