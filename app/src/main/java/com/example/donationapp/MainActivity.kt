2e1c5029f0d701ebed7edaa48905307c808eb3d7
Bandhan singh <parbhatbandhan@gmail.com>
2021-08-10 00:43:01 +0530

    removed unnecessary code

--- app/src/main/java/com/example/donationapp/MainActivity.kt
+++ app/src/main/java/com/example/donationapp/MainActivity.kt
@@ -20,20 +20,17 @@ class MainActivity : AppCompatActivity(), PaymentResultListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         Checkout.preload(applicationContext)
 
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
-
-
+        //opening checkout at the click of button
         binding.donateButton.setOnClickListener {
             makeDonation()
         }
 
         //opens web payment page
         binding.donateMail.setOnClickListener {
-
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://rzp.io/l/aJZrSyWI")))
         }
     }
@@ -41,8 +38,9 @@ class MainActivity : AppCompatActivity(), PaymentResultListener {
     private fun makeDonation() {
 
         val activity: Activity = this
-        val co = Checkout()
+        val co = Checkout()                 //instance of Checkout
 
+        //passing the payment details and options as a JSONObject
         try {
             val options = JSONObject()
             options.put("name","Bandhan Corp.")
@@ -50,15 +48,16 @@ class MainActivity : AppCompatActivity(), PaymentResultListener {
             options.put("image","https://s3.amazonaws.com/rzp-mobile/images/rzp.png")
             options.put("theme.color", "#4CAF50")
             options.put("currency","INR")
-            options.put("amount","60000")
-            options.put("send_sms_hash",true);
+            options.put("amount","30000")
+            options.put("send_sms_hash",true)
 
             val prefill = JSONObject()
             prefill.put("email","parbhatbandhan@gmail.com")
             prefill.put("contact","6006246956")
 
             options.put("prefill",prefill)
-            co.open(activity,options)
+            co.open(activity,options)                      //launches the Checkout form where the customer completes the payment
+
         }catch (e: Exception){
             Toast.makeText(activity,"Error in payment: "+ e.message,Toast.LENGTH_LONG).show()
             e.printStackTrace()
